4b63b1166bb5a1435bbb67fac053fe39db1ad5a0
Serhii-Nikitin <serg.niki@i.ua>
2021-03-29 21:08:45 +0300

    Updates

---
+++ precache-manifest.ecdf6d43c0b095946dec1cbd147e7a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c18786d249b313c7aabde8556f28ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8cde77543a288cbf8ae2",
+    "url": "./static/css/main.5450e98e.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cde77543a288cbf8ae2",
+    "url": "./static/js/main.846e53aa.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
